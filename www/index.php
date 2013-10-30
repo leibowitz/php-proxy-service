23159fd01ad8c72065ea137ca5332901825258f7
Gianni Moschini <gianni.moschini@hailocab.com>
2013-10-30 19:04:59 +0000

    Save requests to mongo

--- www/index.php
+++ www/index.php
@@ -11,6 +11,27 @@ $app = new Silex\Application();
 $app['debug'] = true;
 $app['domain'] = 'proxy.dev';
 
+
+class MyListener {
+    function postConnect() {
+        //print_r(func_get_args());
+    }
+}
+
+$app->register(new SilexMongo\MongoDbExtension(), array(
+    'mongodb.connection'    => array(
+        'server' => 'localhost:17017',
+        'configuration' => function($configuration) {
+            $configuration->setLoggerCallable(function($logs) {
+                //print_r($logs);
+            });
+        },
+        'eventmanager' => function($eventmanager) {
+            $eventmanager->addEventListener('postConnect', new MyListener());
+        }
+    )
+));
+
 $app->match('{url}', function($url, Request $request) use ($app) {
 
     $host = str_replace('.'.$app['domain'], '', $request->getHost());
@@ -26,19 +47,47 @@ $app->match('{url}', function($url, Request $request) use ($app) {
         )
     );
 
+    //$parts = explode('.', $host);
+
     $client = new Client($url, array(
         'request.options' => array(
             'exceptions' => false
         )
     ));
-    $proxyRequest = $client->createRequest($request->getMethod(), $url);
-    if($request->getMethod() != 'GET') {
+
+    $headers = $request->headers->all();
+    $headers['host'] = $host;
+
+    $proxyRequest = $client->createRequest($request->getMethod(), $url, $headers, $request->getContent());
+
+    /*if($request->getMethod() != 'GET') {
         $proxyRequest->setBody($request->getContent());
-    }
+    }*/
 
     $proxyResponse = $proxyRequest->send();
 
+    $document = array(
+        'request' =>
+            array(
+                'body' => $request->getContent(),
+                //'headers' => $request->headers->all(),
+                'headers' => $proxyRequest->getHeaders()->toArray(),
+                'host' => $proxyRequest->getHost(),
+                'path' => $proxyRequest->getPath().'?'.$proxyRequest->getQuery(true),
+            ),
+        'response' =>
+            array(
+                'body' => $proxyResponse->getBody(true),
+                'headers' => $proxyResponse->getHeaders()->toArray(),
+            )
+    );
+
+    $db = $app['mongodb']->selectCollection('proxyservice', 'log');
+    $db->insert($document);
+
+
     $response = new Response($proxyResponse->getBody());
+
     return $response;
 })->assert('url', '.*');
 
