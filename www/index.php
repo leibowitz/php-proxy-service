e5dedc8dddfc2e4628b82bf1a9cebccb9b9f6bfd
Gianni Moschini <gianni.moschini@hailocab.com>
2013-12-22 10:44:36 +0000

    Handle mock.dev requests sending recorded content

--- www/index.php
+++ www/index.php
@@ -10,6 +10,7 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 $app['domain'] = 'proxy.dev';
+$app['domains'] = array('proxy.dev', 'mock.dev');
 
 $app['removeHeaders'] = array(
         'Content-Length',
@@ -17,6 +18,41 @@ $app['removeHeaders'] = array(
         'Content-Encoding'
     );
 
+function fix_headers($headers)
+{
+    return array_flatten_values(filter_headers($headers));
+}
+
+function array_flatten_values($headers)
+{
+    // Some functions returns each header value as header
+    // Let's flatten a little bit
+    array_walk($headers, function(&$value, $index){
+        $value = is_array($value) ? implode($value) : $value;
+    });
+
+    return $headers;
+}
+
+function filter_headers($headers)
+{
+    global $app;
+
+    // Filter some headers we don't want to store/pass around
+    foreach($app['removeHeaders'] as $field) {
+
+        if( isset($headers[$field]) ) {
+            unset($headers[$field]);
+        }
+
+        if( isset($headers[strtolower($field)]) ) {
+            unset($headers[strtolower($field)]);
+        }
+
+    }
+
+    return $headers;
+}
 
 class MyListener {
     function postConnect() {
@@ -40,23 +76,43 @@ $app->register(new SilexMongo\MongoDbExtension(), array(
 
 $app->match('{url}', function($url, Request $request) use ($app) {
 
+    $host = $request->getHost();
+    $method = $request->getMethod();
+
+    $mock = false;
+
     // Remove the proxy.dev
-    $host = str_replace('.'.$app['domain'], '', $request->getHost());
+    foreach($app['domains'] as $domain) {
+        if($domain == 'mock.dev' && strpos($host, $domain) !== false) {
+            $mock = true;
+        }
+
+        $host = str_replace('.'.$domain, '', $host);
+    }
 
     // Look if we have a port
     $parts = explode('.', $host);
     // Extract host
     $host = $parts[0];
+    $port = 80;
     if( count($parts) > 1 ) {
         $port = (int)$parts[1];
-    } else {
-        $port = 80;
     }
 
     // replace dashes with dots
     $host = str_replace('-', '.', $host);
     // replace double dots (which were double dashes) to a dash
     $host = str_replace('..', '-', $host);
+
+    $path = $request->getPathInfo();
+
+    // Funny enough it's unclear how Guzzle supports x-www-form-urlencoded GET requests
+    // sending the content in the query string does the trick for now
+    if($request->getQueryString()) {
+        $query = $request->getQueryString();
+    } else if( $method == 'GET' && $request->headers->get('content-type') == 'application/x-www-form-urlencoded' ) {
+        $query = $request->getContent();
+    }
 
     $url = http_build_url(
         $request->getSchemeAndHttpHost(),
@@ -65,65 +121,93 @@ $app->match('{url}', function($url, Request $request) use ($app) {
             'scheme' => $port == 443 ? 'https' : 'http',
             'host' => $host,
             'path' => $request->getPathInfo(),
-            'query' => $request->getQueryString()
+            'query' => $query
         )
     );
 
+    if($mock) {
+        $criteria = array(
+            'request.host' => $host,
+            //'request.path' => new MongoRegex("^".$path),
+            'request.path' => $path,
+            'response.status' => 200,
+            'request.method' => $method,
+            //'request.body' => $request->getContent(),
+        );
+
+        $db = $app['mongodb']->selectCollection('proxyservice', 'log');
+
+        $cursor = $db->find($criteria)
+            ->sort(array('request.date'=> -1))
+            ->limit(1);
+
+        $data = $cursor->getNext();
+
+        if($data) {
+            $headers = fix_headers($data['response']['headers']);
+
+            $response = new Response($data['response']['body'], $data['response']['status'], $headers);
+            return $response;
+        } else {
+            $response = doRequest($app, $request, $host, $path, $url);
+        }
+
+    } else {
+        $response = doRequest($app, $request, $host, $path, $url);
+    }
+
+    return $response;
+})->assert('url', '.*');
+
+
+function doRequest($app, $request, $host, $path, $url) {
     $client = new Client($url, array(
         'request.options' => array(
             'exceptions' => false
         )
     ));
 
-    $headers = $request->headers->all();
+    $headers = fix_headers($request->headers->all());
+
     $headers['host'] = $host;
 
-    $proxyRequest = $client->createRequest($request->getMethod(), $url, $headers, $request->getContent());
+    $method = $request->getMethod();
 
-    /*if($request->getMethod() != 'GET') {
-        $proxyRequest->setBody($request->getContent());
-    }*/
+    $proxyRequest = $client->createRequest($method, $url, $headers, $request->getContent());
 
     PHP_Timer::start();
     $proxyResponse = $proxyRequest->send();
+
     $time = PHP_Timer::stop();
 
     $document = array(
         'request' =>
             array(
                 'body' => $request->getContent(),
+                'method' => $method,
                 //'headers' => $request->headers->all(),
-                'headers' => $proxyRequest->getHeaders()->toArray(),
+                'headers' => array_flatten_values($proxyRequest->getHeaders()->toArray()),
                 'host' => $proxyRequest->getHost(),
-                'path' => $proxyRequest->getPath().'?'.$proxyRequest->getQuery(true),
+                'path' => $path,
                 'time' => $time,
                 'date' => new MongoDate(),
             ),
         'response' =>
             array(
                 'body' => $proxyResponse->getBody(true),
-                'headers' => $proxyResponse->getHeaders()->toArray(),
+                'headers' => array_flatten_values($proxyResponse->getHeaders()->toArray()),
+                'status' => $proxyResponse->getStatusCode(),
             )
     );
+
 
     $db = $app['mongodb']->selectCollection('proxyservice', 'log');
     $db->insert($document);
 
     // Remove a few headers from the response like content-length
-    $headers = $proxyResponse->getHeaders()->toArray();
+    $headers = fix_headers($proxyResponse->getHeaders()->toArray());
 
-    foreach($app['removeHeaders'] as $field) {
-
-        if( !isset($headers[$field]) ) {
-            continue;
-        }
-
-        unset($headers[$field]);
-    }
-
-    $response = new Response($proxyResponse->getBody(), $proxyResponse->getStatusCode(), $headers);
-
-    return $response;
-})->assert('url', '.*');
+    return new Response($proxyResponse->getBody(), $proxyResponse->getStatusCode(), $headers);
+}
 
 $app->run();
